daf64d737b642cb2a87d1e35b29ae2a2ef0f404a
alwaysquietll <alwaysquietll@gmail.com>
2019-04-03 08:27:27 +0800

    2019/4/3 8:27 Chapter2 UI

--- Chapter2/2.5/ArrayAdapterTest/app/src/main/java/com/jack/arrayadaptertest/MainActivity.java
+++ Chapter2/2.5/ArrayAdapterTest/app/src/main/java/com/jack/arrayadaptertest/MainActivity.java
@@ -15,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
         String[] arr1 = {"孙悟空","猪八戒","牛魔王"};
         ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, R.layout.array_item, arr1);
         list1.setAdapter(adapter1);
+
+        ListView list2 = (ListView) findViewById(R.id.list2);
+        String[] arr2 = {"Java", "Hibernate", "Spring", "Android"};
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this,R.layout.checked_item, arr2);
+        list2.setAdapter(adapter2);
     }
 }
