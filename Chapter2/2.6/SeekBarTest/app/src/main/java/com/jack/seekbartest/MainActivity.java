e1040cf0d12ff9e3c5d31b4411a909b65992b204
alwaysquietll <alwaysquietll@gmail.com>
2019-04-15 20:29:31 +0800

    2019/4/15 20:29 Chapter2 UI

---
+++ Chapter2/2.6/SeekBarTest/app/src/main/java/com/jack/seekbartest/MainActivity.java
@@ -0,0 +1,33 @@
+package com.jack.seekbartest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final ImageView imageView = (ImageView) findViewById(R.id.image);
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekbar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                imageView.setImageAlpha(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
