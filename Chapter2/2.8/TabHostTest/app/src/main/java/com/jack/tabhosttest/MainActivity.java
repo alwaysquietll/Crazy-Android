a725e290611d74c441487ba4b203eadd6a5a8732
jack <alwaysquietll@gmail.com>
2019-04-27 20:21:44 +0800

    2019/4/27 20:21 Chapter2 UI

---
+++ Chapter2/2.8/TabHostTest/app/src/main/java/com/jack/tabhosttest/MainActivity.java
@@ -0,0 +1,29 @@
+package com.jack.tabhosttest;
+
+import android.app.TabActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class MainActivity extends TabActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TabHost tabHost = getTabHost();
+        TabHost.TabSpec tab1 = tabHost.newTabSpec("tab1")
+                .setIndicator("已接电话")
+                .setContent(R.id.tab01);
+        tabHost.addTab(tab1);
+        TabHost.TabSpec tab2 = tabHost.newTabSpec("tab2")
+                .setIndicator("呼出电话", getResources()
+                .getDrawable(R.drawable.ic_launcher))
+                .setContent(R.id.tab02);
+        tabHost.addTab(tab2);
+        TabHost.TabSpec tab3 = tabHost.newTabSpec("tab3")
+                .setIndicator("未接电话")
+                .setContent(R.id.tab03);
+        tabHost.addTab(tab3);
+    }
+}
