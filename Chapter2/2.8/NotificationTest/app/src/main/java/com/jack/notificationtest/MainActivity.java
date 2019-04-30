ffb097291baf86a98814815b01fc18bd711d3963
jack <alwaysquietll@gmail.com>
2019-04-30 10:36:24 +0800

    2019/4/30 10:36 Chapter2 UI

--- Chapter2/2.8/NotificationTest/app/src/main/java/com/jack/notificationtest/MainActivity.java
+++ Chapter2/2.8/NotificationTest/app/src/main/java/com/jack/notificationtest/MainActivity.java
@@ -1,11 +1,13 @@
 package com.jack.notificationtest;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
@@ -20,100 +22,53 @@ import static android.content.ContentValues.TAG;
 
 public class MainActivity extends Activity {
 
-    String id = "channel_001";
-    String name = "name";
-    Button btn;
-    static final int NOTIFICATION_ID =  0x123;
-    NotificationManager nm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        nm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
-        String id = "my_channel_01";
-        String name="我是渠道名字";
-        nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        btn = (Button) findViewById(R.id.send);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Notification notification = null;
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    NotificationChannel mChannel = new NotificationChannel(id, name, NotificationManager.IMPORTANCE_LOW);
-                    Toast.makeText(this, mChannel.toString(), Toast.LENGTH_SHORT).show();
-                    Log.i(TAG, mChannel.toString());
-                    nm.createNotificationChannel(mChannel);
-                    notification = new Notification.Builder(this)
-                            .setChannelId(id)
-                            .setContentTitle("5 new messages")
-                            .setContentText("hahaha")
-                            .setSmallIcon(R.mipmap.ic_launcher).build();
-                } else {
-                    NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                            .setContentTitle("5 new messages")
-                            .setContentText("hahaha")
-                            .setSmallIcon(R.mipmap.ic_launcher)
-                            .setOngoing(true);
-                    //.setChannel(id);//无效
-                    notification = notificationBuilder.build();
-                }
-                nm.notify(111123, notification);
-            }
-        });
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId = "chat";
+            String channelName = "聊天消息";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            createNotificationChannel(channelId, channelName, importance);
+
+            channelId = "subscribe";
+            channelName = "订阅消息";
+            importance = NotificationManager.IMPORTANCE_DEFAULT;
+            createNotificationChannel(channelId, channelName, importance);
+        }
 
     }
-//    public void send(View source)
-//    {
-//        System.out.println(111);
-//        Intent intent = new Intent(MainActivity.this, OtherActivity.class);
-//        PendingIntent pi = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
-//        Notification notify = new Notification.Builder(this)
-//                // 设置打开该通知，该通知自动消失
-//                .setAutoCancel(true)
-//                // 设置显示在状态栏的通知提示信息
-//                .setTicker("有新消息")
-//                // 设置通知的图标
-//                .setSmallIcon(R.drawable.notify)
-//                // 设置通知内容的标题
-//                .setContentTitle("一条新通知")
-//                // 设置通知内容
-//                .setContentText("恭喜你，您加薪了，工资增加20%!")
-//                // 设置使用系统默认的声音、默认LED灯
-//                // .setDefaults(Notification.DEFAULT_SOUND
-//                // |Notification.DEFAULT_LIGHTS)
-//                // 设置通知的自定义声音
-//                //.setSound(Uri.parse("android.resource://org.crazyit.ui/"
-//                //        + R.raw.msg))
-//                .setWhen(System.currentTimeMillis())
-//                // 设改通知将要启动程序的Intent
-//                .setContentIntent(pi)  // ①
-//                .build();
-//        // 发送通知
-//        Notification notification = null;
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            NotificationChannel mChannel = new NotificationChannel(id, name, NotificationManager.IMPORTANCE_LOW);
-//            Toast.makeText(this, mChannel.toString(), Toast.LENGTH_SHORT).show();
-//            Log.i(TAG, mChannel.toString());
-//            nm.createNotificationChannel(mChannel);
-//            notification = new Notification.Builder(this)
-//                    .setChannelId(id)
-//                    .setContentTitle("5 new messages")
-//                    .setContentText("hahaha")
-//                    .setSmallIcon(R.mipmap.ic_launcher).build();
-//        } else {
-//            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-//                    .setContentTitle("5 new messages")
-//                    .setContentText("hahaha")
-//                    .setSmallIcon(R.mipmap.ic_launcher)
-//                    .setOngoing(true);
-//                    //.setChannel(id);//无效
-//            notification = notificationBuilder.build();
-//        }
-//        nm.notify(NOTIFICATION_ID, notify);
-//    }
-//    public void del(View v)
-//    {
-//        nm.cancel(NOTIFICATION_ID);
-//        System.out.println(222);
-//    }
+    @TargetApi(Build.VERSION_CODES.O)
+    private void createNotificationChannel(String channelId, String channelName, int importance) {
+        NotificationChannel channel = new NotificationChannel(channelId, channelName, importance);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(
+                NOTIFICATION_SERVICE);
+        notificationManager.createNotificationChannel(channel);
+    }
+    public void send(View view) {
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification notification = new NotificationCompat.Builder(this, "chat")
+                .setContentTitle("收到一条聊天消息")
+                .setContentText("今天中午吃什么？")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.notify)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.notify))
+                .setAutoCancel(true)
+                .build();
+        manager.notify(1, notification);
+    }
+
+    public void subscribe(View view) {
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification notification = new NotificationCompat.Builder(this, "subscribe")
+                .setContentTitle("收到一条订阅消息")
+                .setContentText("地铁沿线30万商铺抢购中！")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.notify)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.notify))
+                .setAutoCancel(true)
+                .build();
+        manager.notify(2, notification);
+    }
 }
