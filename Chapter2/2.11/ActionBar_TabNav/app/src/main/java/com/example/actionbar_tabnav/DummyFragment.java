3434c5805e4cf51bd34435b32a29e92d6b7f4adb
jack <alwaysquietll@gmail.com>
2020-06-25 22:18:56 +0800

    Chapter2 UI ActionBar_TabNav

---
+++ Chapter2/2.11/ActionBar_TabNav/app/src/main/java/com/example/actionbar_tabnav/DummyFragment.java
@@ -0,0 +1,25 @@
+package com.example.actionbar_tabnav;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+
+public class DummyFragment extends Fragment {
+    public static final String ARG_SECTION_NUMBER = "section_number";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setGravity(Gravity.START);
+        Bundle args = getArguments();
+        textView.setText(args.getInt(ARG_SECTION_NUMBER) + "");
+        textView.setTextSize(30);
+        return textView;
+    }
+}
