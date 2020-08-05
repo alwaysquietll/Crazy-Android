e968d9f3e6b5d8aedcabf2e8df7cd411f68ef8d5
jack <alwaysquietll@gmail.com>
2020-08-05 17:31:35 +0800

    Chapter4 Activity Fragment StartActivity

---
+++ Chapter4/4.1/StartActivity/app/src/main/java/com/example/startactivity/SecondActivity.java
@@ -0,0 +1,33 @@
+package com.example.startactivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second);
+        Button previous = (Button)findViewById(R.id.previous);
+        Button close = (Button)findViewById(R.id.close);
+        previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
